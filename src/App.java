d1179364f77df186fe4b75694a19d99470333d07
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2022-12-01 02:53:03 +0700

    feat: add file fxml & controller

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FlappyBird.fxml"));
+        Scene scene = new Scene(root);
+        scene.getRoot().requestFocus();
+        stage.setTitle("Flappy Bird!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 }
